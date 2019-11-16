68567b342ec43d474aca3e6cb5bd8906e5270347
Markus Petrykowski <github@markus-petrykowski.de>
2019-11-16 11:54:58 +0200

    fix dependiencies

--- wecoshop-frontend/src/Pages/PartyOverview/index.js
+++ wecoshop-frontend/src/Pages/PartyOverview/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import api from "../../api";
 import Title from "../../Components/Title";
 import Page from "../../Components/Page";
 
 export default class PartyOverview extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    api.parties().then(parties => {
+      this.setState({
+        parties,
+        loading: false
+      });
+    });
+  }
+
   render() {
     return (
       <Page>
