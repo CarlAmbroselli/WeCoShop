6d1e2ecc93a58d5866357b515255d69cdca1e2be
Carl Ambroselli <git@carl-ambroselli.de>
2019-11-16 14:13:06 +0200

    Add search

--- routes/search.js
+++ routes/search.js
@@ -6,8 +6,27 @@ let search = {
       lat: 60.1733244,
       lon: 24.9410248
     })
-  }
-  
+  },
+
+  searchItem(req, res) {
+    req.send([
+      {
+          id: 1,
+          name: "Tank Top",
+          pictureUrl: "https://picsum.photos/50/50",
+          price: 12,
+          currency: "EUR"
+      },
+      {
+          id: 2,
+          name: "Jeans",
+          pictureUrl: "https://picsum.photos/50/50",
+          price: 12,
+          currency: "EUR"
+      }
+    ])
+}
+
 }
 
 module.exports = search
