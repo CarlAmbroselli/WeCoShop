55308c5bb9297bf15e05f1a7a5cce35eb38f4271
Markus Petrykowski <github@markus-petrykowski.de>
2019-11-16 11:44:17 +0200

    first version of PartyOverview

---
+++ wecoshop-frontend/config-overrides.js
@@ -0,0 +1,21 @@
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+// module.exports = function override(config, env) {
+//   // do stuff with the webpack config...
+//   return config;
+// };
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@primary-color": "#C8D6FC",
+      "@text-color-secondary": "#D5E3FF"
+    }
+  })
+);
---
+++ wecoshop-frontend/src/Pages/PartyOverviev/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Title from "../../Components/Title";
+import Page from "../../Components/Page";
+
+export default class PartyOverview extends React.Component {
+  render() {
+    return (
+      <Page>
+        <Title>Parties</Title>
+        {/* <Button type="primary">Hallo</Button> */}
+      </Page>
+    );
+  }
+}
